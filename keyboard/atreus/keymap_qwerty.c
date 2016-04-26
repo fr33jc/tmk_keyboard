ed766bbc0e523d3219a1e2e8815d8b4229b22a67
fr33jc <fr33jc@gmail.com>
2016-04-26 16:14:49 -0700

    Relocate backspace and backslash

--- keyboard/atreus/keymap_qwerty.c
+++ keyboard/atreus/keymap_qwerty.c
@@ -11,14 +11,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
          KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, \
          KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, \
          KC_TAB, KC_GRAVE, KC_EQUAL, KC_LGUI, KC_LSFT, KC_FN4, KC_ENT, \
-         KC_SPC, KC_FN0, KC_MINS, KC_QUOT, KC_BSPC),
+         KC_SPC, KC_FN0, KC_MINS, KC_QUOT, KC_BSLS),
   /* 1: punctuation and numbers */
   KEYMAP(KC_1, KC_2, KC_3, KC_4, KC_5, \
            KC_6, KC_7, KC_8, KC_9, KC_0, \
-         KC_HOME, KC_UP, KC_END, KC_PGUP, KC_NO, \
-           KC_NO, KC_NO, KC_NO, KC_LBRC, KC_RBRC, \
+         KC_HOME, KC_UP, KC_END, KC_PGUP, KC_BSPC, \
+           KC_BSPC, KC_NO, KC_NO, KC_LBRC, KC_RBRC, \
          KC_LEFT, KC_DOWN, KC_RIGHT, KC_PGDN, KC_NO, \
-           KC_NO, KC_NO, KC_NO, KC_DOT, KC_BSLS,    \
+           KC_NO, KC_NO, KC_NO, KC_DOT, KC_NO, \
          KC_FN1, KC_NO, KC_NO, KC_LGUI, KC_LSFT, KC_LCTL, KC_ENT,
            KC_SPC, KC_FN0, KC_NO, KC_NO, KC_BSPC),
   /* 2: arrows and function keys */
