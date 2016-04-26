341d61f213910d21ce7e3235ce600e95efbc58a4
fr33jc <fr33jc@gmail.com>
2016-04-25 21:01:41 -0700

    Adjust qwerty layout to be more vim-friendly

--- keyboard/atreus/keymap_qwerty.c
+++ keyboard/atreus/keymap_qwerty.c
@@ -10,10 +10,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KEYMAP(KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, \
          KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, \
          KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, \
-         KC_ESC, KC_TAB, KC_LGUI, KC_LSFT, KC_BSPC, KC_LCTL, KC_LALT,     \
-         KC_SPC, KC_FN0, KC_MINS, KC_QUOT, KC_ENT),                     \
+         KC_TAB, KC_GRAVE, KC_EQUAL, KC_LGUI, KC_LSFT, KC_FN4, KC_ENT, \
+         KC_SPC, KC_FN0, KC_MINS, KC_QUOT, KC_BSPC),
   /* 1: punctuation and numbers */
-  FN_ARROW_LAYER,                                     \
+  KEYMAP(KC_1, KC_2, KC_3, KC_4, KC_5, \
+           KC_6, KC_7, KC_8, KC_9, KC_0, \
+         KC_HOME, KC_UP, KC_END, KC_PGUP, KC_NO, \
+           KC_NO, KC_NO, KC_NO, KC_LBRC, KC_RBRC, \
+         KC_LEFT, KC_DOWN, KC_RIGHT, KC_PGDN, KC_NO, \
+           KC_NO, KC_NO, KC_NO, KC_DOT, KC_BSLS,    \
+         KC_FN1, KC_NO, KC_NO, KC_LGUI, KC_LSFT, KC_LCTL, KC_ENT,
+           KC_SPC, KC_FN0, KC_NO, KC_NO, KC_BSPC),
   /* 2: arrows and function keys */
   LAYER_TWO
 };
@@ -22,7 +29,8 @@ const uint16_t PROGMEM fn_actions[] = {
   [0] = ACTION_LAYER_MOMENTARY(1),  // to Fn overlay
   [1] = ACTION_LAYER_ON(2, 1),  // switch to layer 2
   [2] = ACTION_LAYER_OFF(2, 1),  // switch back to layer 0
-  [3] = ACTION_FUNCTION(BOOTLOADER)
+  [3] = ACTION_FUNCTION(BOOTLOADER),
+  [4] = ACTION_MODS_TAP_KEY(MOD_LCTL, KC_ESC)  // hold ctrl, tap esc
 };
 
 void action_function(keyrecord_t *record, uint8_t id, uint8_t opt)
