a726c6cb12472672e2524b3a4de142673ec1df6a
fr33jc <fr33jc@gmail.com>
2016-05-21 17:28:49 -0700

    Hold for L-ALT, tap for Enter

--- keyboard/atreus/keymap_qwerty.c
+++ keyboard/atreus/keymap_qwerty.c
@@ -10,7 +10,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KEYMAP(KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, \
          KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, \
          KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, \
-         KC_TAB, KC_GRAVE, KC_EQUAL, KC_LGUI, KC_LSFT, KC_FN4, KC_ENT, \
+         KC_TAB, KC_GRAVE, KC_EQUAL, KC_LGUI, KC_LSFT, KC_FN4, KC_FN5, \
          KC_SPC, KC_FN0, KC_MINS, KC_QUOT, KC_BSLS),
   /* 1: punctuation and numbers */
   KEYMAP(KC_1, KC_2, KC_3, KC_4, KC_5, \
@@ -30,7 +30,8 @@ const uint16_t PROGMEM fn_actions[] = {
   [1] = ACTION_LAYER_ON(2, 1),  // switch to layer 2
   [2] = ACTION_LAYER_OFF(2, 1),  // switch back to layer 0
   [3] = ACTION_FUNCTION(BOOTLOADER),
-  [4] = ACTION_MODS_TAP_KEY(MOD_LCTL, KC_ESC)  // hold ctrl, tap esc
+  [4] = ACTION_MODS_TAP_KEY(MOD_LCTL, KC_ESC),  // hold L-CTRL, tap ESC
+  [5] = ACTION_MODS_TAP_KEY(MOD_LALT, KC_ENT)  // hold L-ALT, tap ENTER
 };
 
 void action_function(keyrecord_t *record, uint8_t id, uint8_t opt)
