c8ca3af9b50a65b8f6aecf097f614ed2039cea99
fr33jc <fr33jc@gmail.com>
2016-07-07 11:07:44 -0700

    Add another FN and layer for grave, dash, equals, etc...

--- keyboard/atreus/keymap_qwerty.c
+++ keyboard/atreus/keymap_qwerty.c
@@ -7,22 +7,22 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   /* 0: mostly letters */
-  KEYMAP(KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, \
-         KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, \
-         KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, \
-         KC_TAB, KC_GRAVE, KC_EQUAL, KC_LGUI, KC_LSFT, KC_FN4, KC_FN5, \
-         KC_SPC, KC_FN0, KC_MINS, KC_QUOT, KC_BSLS),
-  /* 1: punctuation and numbers */
-  KEYMAP(KC_1, KC_2, KC_3, KC_4, KC_5, \
-           KC_6, KC_7, KC_8, KC_9, KC_0, \
-         KC_HOME, KC_UP, KC_END, KC_PGUP, KC_BSPC, \
-           KC_BSPC, KC_NO, KC_NO, KC_LBRC, KC_RBRC, \
-         KC_LEFT, KC_DOWN, KC_RIGHT, KC_PGDN, KC_NO, \
-           KC_NO, KC_NO, KC_NO, KC_DOT, KC_NO, \
-         KC_FN1, KC_NO, KC_NO, KC_LGUI, KC_LSFT, KC_LCTL, KC_ENT,
-           KC_SPC, KC_FN0, KC_NO, KC_NO, KC_BSPC),
+  KEYMAP(KC_Q, KC_W, KC_E, KC_R, KC_T,                           KC_Y, KC_U, KC_I, KC_O, KC_P, \
+         KC_A, KC_S, KC_D, KC_F, KC_G,                           KC_H, KC_J, KC_K, KC_L, KC_SCLN, \
+         KC_Z, KC_X, KC_C, KC_V, KC_B,                           KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, \
+         KC_TAB, KC_NO, KC_NO, KC_LGUI, KC_LSFT, KC_FN4, KC_FN5, KC_SPC, KC_FN0, KC_FN6, KC_NO, KC_BSLS),
+  /* 1: quotes, hyphen, equals */
+  KEYMAP(KC_GRAVE, KC_NO, KC_NO, KC_NO, KC_NO,                   KC_NO, KC_NO, KC_NO, KC_MINS, KC_EQUAL, \
+         KC_HOME, KC_UP, KC_END, KC_PGUP, KC_BSPC,               KC_BSPC, KC_NO, KC_NO, KC_NO, KC_QUOT, \
+         KC_LEFT, KC_DOWN, KC_RIGHT, KC_PGDN, KC_NO,             KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, \
+         KC_TAB, KC_NO, KC_NO, KC_LGUI, KC_LSFT, KC_FN4, KC_FN5, KC_NO, KC_FN0, KC_NO, KC_NO, KC_NO),
   /* 2: arrows and function keys */
-  LAYER_TWO
+  LAYER_TWO,
+  /* 3: punctuation and numbers */
+  KEYMAP(KC_1, KC_2, KC_3, KC_4, KC_5,                           KC_6, KC_7, KC_8, KC_9, KC_0, \
+         KC_HOME, KC_UP, KC_END, KC_PGUP, KC_BSPC,               KC_BSPC, KC_NO, KC_NO, KC_LBRC, KC_RBRC, \
+         KC_LEFT, KC_DOWN, KC_RIGHT, KC_PGDN, KC_NO,             KC_NO, KC_NO, KC_NO, KC_DOT, KC_NO, \
+         KC_FN1, KC_NO, KC_NO, KC_LGUI, KC_LSFT, KC_FN4, KC_FN5, KC_SPC, KC_NO, KC_FN6, KC_NO, KC_NO),
 };
 
 const uint16_t PROGMEM fn_actions[] = {
@@ -31,7 +31,8 @@ const uint16_t PROGMEM fn_actions[] = {
   [2] = ACTION_LAYER_OFF(2, 1),  // switch back to layer 0
   [3] = ACTION_FUNCTION(BOOTLOADER),
   [4] = ACTION_MODS_TAP_KEY(MOD_LCTL, KC_ESC),  // hold L-CTRL, tap ESC
-  [5] = ACTION_MODS_TAP_KEY(MOD_LALT, KC_ENT)  // hold L-ALT, tap ENTER
+  [5] = ACTION_MODS_TAP_KEY(MOD_LALT, KC_ENT),  // hold L-ALT, tap ENTER
+  [6] = ACTION_LAYER_MOMENTARY(3),  // to FnB overlay
 };
 
 void action_function(keyrecord_t *record, uint8_t id, uint8_t opt)
